3d8f852bc0fa7401e8009965aee35671f7f11a59
Lucian Roinita <roinitalucian@yahoo.com>
2020-06-12 18:23:57 +0300

    Updates

---
+++ precache-manifest.845a46f19c6363912a91f7b60fb21836.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ccf0d1723ac1e2908723c5298b7a23e3",
+    "url": "/to-do-list/index.html"
+  },
+  {
+    "revision": "43a72fd1f007e89e92bd",
+    "url": "/to-do-list/static/css/main.a7948695.chunk.css"
+  },
+  {
+    "revision": "9c8702f2b1746246220a",
+    "url": "/to-do-list/static/js/2.f6aec62c.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/to-do-list/static/js/2.f6aec62c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "43a72fd1f007e89e92bd",
+    "url": "/to-do-list/static/js/main.1c39a7be.chunk.js"
+  },
+  {
+    "revision": "fa27aabefbb81c21a734",
+    "url": "/to-do-list/static/js/runtime-main.80f3eba2.js"
+  }
+]);
